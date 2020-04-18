7be6b6aeb1a9503a1ed56edda0eda2e8e3245c4e
Nicromano <40124504+Nicromano@users.noreply.github.com>
2020-04-17 21:48:38 -0500

    Error en los arreglos de los documentos Links comments

--- databases/databases.go
+++ databases/databases.go
@@ -20,10 +20,30 @@ var (
 	err    error
 )
 
+type User struct {
+	Username  string        `json:"username"    bson:"username" `
+	Password  string        `json:"password"    bson:"password"`
+	Email     string        `json:"email"    	bson:"email"`
+	Image     string        `json:"image"   	bson:"image"`
+	Links     []Link        `json:"links"    	bson:"links"`
+	Follow    []interface{} `json:"follow"   	bson:"follow"`
+	Followers []interface{} `json:"followers"   bson:"followers"`
+}
+
+/* Estructura de link */
 type Link struct {
-	Name        string `bson:"name"`
-	Url         string `bson:"url"`
-	Description string `bson:"description"`
+	Name        string    `json:"name"  		bson:"name" 	`
+	Url         string    `json:"url"  			bson:"url" 		`
+	Description string    `json:"description"  	bson:"description" `
+	Comments    []Comment `json:"comments"  	bson:"comments"`
+	Like        uint32    `json:"like"  		bson:"like"`
+	Dislike     uint32    `json:"dislike"  		bson:"dislike"`
+}
+type Comment struct {
+	IdUser  string `json:"iduser"  		bson:"iduser"`
+	Content string `json:"content"  	bson:"content"`
+	Like    uint32 `json:"like"  		bson:"like"`
+	Dislike uint32 `json:"dislike"  	bson:"dislike"`
 }
 
 //Conectar a la base de datos
@@ -43,7 +63,7 @@ func Conectar() {
 }
 
 // Insertat un elemento en una coleccion
-func InsertOne(database string, document string, data interface{}) string {
+func InsertOne(database string, document string, data interface{}) error {
 	/* Crea una instancia de la coleccion */
 	collection := client.Database(database).Collection(document)
 	/* Abre un contexto */
@@ -52,14 +72,15 @@ func InsertOne(database string, document string, data interface{}) string {
 	res, err := collection.InsertOne(ctx, convertBSON(data))
 	/*  chequea error*/
 	controlError(err)
-	id := fmt.Sprintf("%s", res.InsertedID)
-	return id
+	fmt.Printf("%v", res.InsertedID)
+	return err
 }
 
 func controlError(err error) {
 	if err != nil {
-		log.Panic(err)
+		log.Println(err)
 	}
+	log.Println("----END OF ERROR----")
 }
 
 //Buscar todos los elementos
@@ -83,13 +104,18 @@ func FindAll(database string, document string) []interface{} {
 }
 
 //Encuentra uno
-func FindOne(database string, document string, find interface{}) interface{} {
-	var result interface{}
+func FindOne(database, document, clave, valor string) interface{} {
+	var result User
 	collection := client.Database(database).Collection(document)
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 
-	err := collection.FindOne(ctx, find).Decode(&result)
-	controlError(err)
+	filter := bson.D{{clave, valor}}
+	err := collection.FindOne(ctx, filter).Decode(&result)
+	if err != nil {
+		fmt.Println("Error: " + err.Error())
+		fmt.Printf("Dato %v no encontrado en la coleccion %s\n", filter, document)
+		return nil
+	}
 
 	return result
 }
@@ -145,7 +171,7 @@ func DeleteOne(database string, document string, busqueda interface{}) {
 
 	collection := client.Database(database).Collection(document)
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	deleteResult, err := collection.DeleteOne(ctx, busqueda)
+	deleteResult, err := collection.DeleteOne(ctx, convertBSON(busqueda))
 	controlError(err)
 	fmt.Printf("Fue eliminado %v de la coleccion %s", deleteResult, document)
 
--- router/router.go
+++ router/router.go
@@ -1,10 +1,12 @@
 package router
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
+	"../crypto"
 	"../databases"
 	"github.com/gorilla/mux"
 )
@@ -21,18 +23,29 @@ type Response struct {
 
 /* Estructura de usuario */
 type User struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Email    string `json:"email"`
-	Image    string `json:"image"`
-	Link     Link   `json:"link"`
+	Username  string        `json:"username"    bson:"username" `
+	Password  string        `json:"password"    bson:"password"`
+	Email     string        `json:"email"    	bson:"email"`
+	Image     string        `json:"image"   	bson:"image"`
+	Links     []Link        `json:"links"    	bson:"links"`
+	Follow    []interface{} `json:"follow"   	bson:"follow"`
+	Followers []interface{} `json:"followers"   bson:"followers"`
 }
 
 /* Estructura de link */
 type Link struct {
-	Name        string `json:"name"`
-	Url         string `json:"url"`
-	Description string `json:"description"`
+	Name        string    `json:"name"  		bson:"name" 	`
+	Url         string    `json:"url"  			bson:"url" 		`
+	Description string    `json:"description"  	bson:"description" `
+	Comments    []Comment `json:"comments"  	bson:"comments"`
+	Like        uint32    `json:"like"  		bson:"like"`
+	Dislike     uint32    `json:"dislike"  		bson:"dislike"`
+}
+type Comment struct {
+	IdUser  string `json:"iduser"  		bson:"iduser"`
+	Content string `json:"content"  	bson:"content"`
+	Like    uint32 `json:"like"  		bson:"like"`
+	Dislike uint32 `json:"dislike"  	bson:"dislike"`
 }
 
 func setupResponse(w *http.ResponseWriter, req *http.Request) {
@@ -47,7 +60,7 @@ func init() {
 	databases.Conectar()
 	router.HandleFunc("/", home).Methods("GET")
 	/* 	router.HandleFunc("/signin", singinUser).Methods(http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch, http.MethodOptions) */
-	router.HandleFunc("/signin", singinUser).Methods("GET")
+	router.HandleFunc("/signin", singinUser).Methods("POST")
 	router.HandleFunc("/signup", signupUser).Methods("POST")
 	router.HandleFunc("/forgot", forgotPassword).Methods("POST")
 	router.HandleFunc("/home", home).Methods("GET")
@@ -78,14 +91,45 @@ func signupUser(w http.ResponseWriter, req *http.Request) {
 	setupResponse(&w, req)
 
 	var user User
+	_ = json.NewDecoder(req.Body).Decode(&user)
 
+	fmt.Printf("%s\n", user)
+
+	findUser := databases.FindOne("links", "User", "username", user.Username)
+	if findUser != nil {
+		//Encontró
+		fmt.Println(findUser)
+		json.NewEncoder(w).Encode(Response{Title: "USER FOUND"})
+	} else {
+		//No encontró
+		err := databases.InsertOne("links", "User", user)
+		if err != nil {
+			//No ingreso datos
+			json.NewEncoder(w).Encode(Response{Title: "UNREGISTERED USER"})
+		} else {
+			//ingresó datos
+			json.NewEncoder(w).Encode(Response{Title: "REGISTERED USER"})
+		}
+	}
 }
+
 func singinUser(w http.ResponseWriter, req *http.Request) {
 	setupResponse(&w, req)
+	var user User
+	_ = json.NewDecoder(req.Body).Decode(&user)
 
-	links := databases.FindAll("links", "links")
+	findUser := databases.FindOne("links", "User", "username", user.Username)
+	fmt.Printf("%v", findUser)
+	if findUser != nil {
+		//Se encontró el usuario
+		//Desencriptar contraseña
 
-	w.Write([]byte(fmt.Sprintf("%s", links)))
+		json.NewEncoder(w).Encode(Response{Title: "USER FOUND"})
+
+	} else {
+		json.NewEncoder(w).Encode(Response{Title: "USER NOT FOUND"})
+
+	}
 
 }
 
@@ -94,15 +138,23 @@ func homeHandler(res http.ResponseWriter, req *http.Request) {
 
 	links := Link{Name: "Google", Url: "https://www.google.com"}
 
-	id := databases.InsertOne("links", "links", links)
-	res.Write([]byte("id ingresado" + id))
+	err := databases.InsertOne("links", "links", links)
+	res.Write([]byte("id ingresado" + err.Error()))
 }
 
 func home(res http.ResponseWriter, req *http.Request) {
 
-	databases.DeleteAll("links", "links")
+	text := "Hola como esta"
 
-	res.Write([]byte("borrada"))
+	hash := crypto.Encrypt(text)
+
+	fmt.Printf("%v\n", hash)
+
+	textPlain := crypto.Decrypt(hash)
+
+	fmt.Printf("%v", textPlain)
+	res.Write([]byte("Hola"))
+
 }
 
 //GetRouter Funcion para retornar el router
